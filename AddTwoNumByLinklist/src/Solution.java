3b4c92d615de6ca9301e9ae6671d84022df0b0cc
coolAutumn <coolAutumn@coolAutumndeMacBook-Pro.local>
2016-03-20 12:10:52 +0800

    Update

--- AddTwoNumByLinklist/src/Solution.java
+++ AddTwoNumByLinklist/src/Solution.java
@@ -25,19 +25,69 @@ public class Solution {
                 carry=false; 
             }
         }
+        else if(l1==null&&l2==null){
+            //to prevent carry can't be added
+            if(carry){
+                num++;
+                carry=false;
+                return new ListNode(num);
+            }
+            else{
+                return null;
+            }
+        }
         else if(l1==null){
+            //to prevent carry can't be added
+            if(carry){
+                l2.val++;
+                if(l2.val>9)
+                {
+                    l2.val=l2.val-10;
+                    l2.next=OneNumCarryForward(l2.next);
+                }
+            }
             return l2;
         }
-        else if(l2==null){
+        else{
+             //to prevent carry can't be added
+            if(carry){
+                l1.val++;
+                if(l1.val>9)
+                {
+                    l1.val=l1.val-10;
+                    l1.next=OneNumCarryForward(l1.next);
+                }
+            }
             return l1;
         }
-        else{
-            return null;
-        }
-        
         //use iteration to link all nodes together
         ListNode listnode=new ListNode(num);
         listnode.next=addTwoNumbers(l1.next,l2.next);
         return listnode;
+    }
+    
+    public ListNode OneNumCarryForward(ListNode l1)
+    {
+        if(l1==null){
+            if(carry){
+                carry=false;
+                return new ListNode(1);
+            }
+            return null;
+        }
+        else{
+            if(carry)
+            {
+                l1.val++;
+                carry=false;
+                if(l1.val>9)
+                {
+                    carry=true;
+                    l1.val=l1.val-10;
+                    l1.next=OneNumCarryForward(l1.next);
+                }
+            }
+            return l1;
+        }
     }
 }
