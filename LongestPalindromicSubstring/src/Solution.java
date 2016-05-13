78cdb67f1a7b29553e66dcfc429aa1da8a7e1767
coolAutumn <autumn3376@gmail.com>
2016-05-13 16:59:38 +0800

    5.../331 05/12/2016

---
+++ LongestPalindromicSubstring/src/Solution.java
@@ -0,0 +1,13 @@
+public class Solution {
+    public String longestPalindrome(String s) {
+        int pos=0;
+        int length=s.length;
+        for(int i=0;i<length;i++){
+            
+        }
+    }
+
+    public String find(String s){
+
+    }
+}
